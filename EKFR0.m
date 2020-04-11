b33fce889ccb7d74f4724ead898fc4f021d48bed
agusisma <62523361+agusisma@users.noreply.github.com>
2020-04-11 23:30:33 +0200

    Update EKFR0.m

--- EKFR0.m
+++ EKFR0.m
@@ -1,23 +1,22 @@
 %% Research code by Agus Hasan
 
-% This code is used to estimate the value of basic reproduction number (R0) based on Extended Kalman Filter
-% The data were taken from 14.02 until 30.03
+% This code is used to estimate the value of R0 based on Extended Kalman Filter
 
 clear;
 clc;
 
 %%
-load TUNR01.txt; % Assuming CFR 1%
-load TUNR02.txt; % Assuming CFR 4%
+load TUNR0.txt;
 
 %%
-tf  = 45;
+tf  = length(TUNR0);
+N   = sum(TUNR0(1,:));                    % number of population
+CFR = TUNR0(end,4)/(sum(TUNR0(end,2:4))); % case fatality rate
+td  = datetime(2020,1,30) + caldays(1:tf);
+Ti  = 14;                                 % infection time
+
 dt  = 0.01;
 t   = dt:dt:tf;
-td  = datetime(2020,2,14) + caldays(1:tf);
-N   = 264000000;                % number of population
-CFR = 0.04;                     % death rate 4%
-Ti  = 10;                       % infection time
 
 %% Measurement matrix
 
@@ -27,12 +26,12 @@ C = [1 0 0 0 0;
      0 0 0 1 0];
 
 %% Noise
-QF = 0.001*eye(5);
-RF = 0.001*eye(4);
+QF = 0.01*eye(5);
+RF = 1*eye(4);
 
 %% Initialization
 xhat     = [N-1; 1; 0; 0; 1]; % initial condition 14.02
-Pplus    = 0*eye(5);
+Pplus    = 1*eye(5);
 
 %% Paramater
 gamma = (1-CFR)*(1/Ti);
@@ -43,9 +42,8 @@ kappa = CFR*1/Ti;
 xArray     = [];
 xhatArray  = [];
 
-%%
-% Simulation
-for i=1:(tf/dt)
+%% Simulation
+for i=1:((tf-1)/dt)
      xhatArray = [xhatArray xhat]; 
      
      % prediction
@@ -70,68 +68,86 @@ for i=1:(tf/dt)
     
     KF    = Pmin*C'*inv(C*Pmin*C'+RF);
     
-    % insert data/measurement
-%     y = [interp1(0:1:tf,TUNR01(:,1),t);
-%          interp1(0:1:tf,TUNR01(:,2),t);
-%          interp1(0:1:tf,TUNR01(:,3),t);
-%          interp1(0:1:tf,TUNR01(:,4),t)];
-    y = [interp1(0:1:tf,TUNR02(:,1),t);
-         interp1(0:1:tf,TUNR02(:,2),t);
-         interp1(0:1:tf,TUNR02(:,3),t);
-         interp1(0:1:tf,TUNR02(:,4),t)];
+    y = [interp1(0:1:tf-1,TUNR0(:,1),t);
+         interp1(0:1:tf-1,TUNR0(:,2),t);
+         interp1(0:1:tf-1,TUNR0(:,3),t);
+         interp1(0:1:tf-1,TUNR0(:,4),t)];
      
-    xhat      = xhat + KF*(y(:,i)-C*xhat);
+    xhat  = xhat + KF*(y(:,i)-C*xhat);
     Pplus = (eye(5)-KF*C)*Pmin;
 end
 
+%% Plotting
+
+xhatSArray = [];
+xhatS      = xhatArray(1,tf);
+xhatIArray = [];
+xhatI      = xhatArray(2,tf);
+xhatHArray = [];
+xhatH      = xhatArray(3,tf);
+xhatDArray = [];
+xhatD      = xhatArray(4,tf);
+xhatRArray = [];
+xhatR      = xhatArray(5,tf);
+for i=1:tf-1
+    xhatSArray = [xhatSArray xhatS];
+    xhatS      = xhatArray(1,100*i);
+    xhatIArray = [xhatIArray xhatI];
+    xhatI      = xhatArray(2,100*i);
+    xhatHArray = [xhatHArray xhatH];
+    xhatH      = xhatArray(3,100*i);
+    xhatDArray = [xhatDArray xhatD];
+    xhatD      = xhatArray(4,100*i);
+    xhatRArray = [xhatRArray xhatR];
+    xhatR      = xhatArray(5,100*i);
+end
+
 figure(1)
-subplot(4,1,1)
-plot(t,xhatArray(1,:),'LineWidth',3)
+subplot(2,1,1)
+plot(td,[xhatSArray TUNR0(end,1)],'LineWidth',6)
 hold on
-%plot(0:1:tf,TUNR01(:,1),'*r','LineWidth',3)
-plot(0:1:tf,TUNR02(:,1),'*r','LineWidth',3)
+plot(td,TUNR0(:,1),'*r','LineWidth',6)
 ylabel('S')
-title('Model SIRD')
 set(gca,'FontSize',24)
+legend('Estimasi','Data Lapangan')
 grid on
 grid minor
-subplot(4,1,2)
-plot(t,xhatArray(2,:),'LineWidth',3)
+subplot(2,1,2)
+plot(td,[xhatIArray TUNR0(end,2)],'LineWidth',6)
 hold on
-%plot(0:1:tf,TUNR01(:,2),'*r','LineWidth',3)
-plot(0:1:tf,TUNR02(:,2),'*r','LineWidth',3)
+plot(td,TUNR0(:,2),'*r','LineWidth',6)
 ylabel('I')
-set(gca,'FontSize',24)
-grid on
-grid minor
-subplot(4,1,3)
-plot(t,xhatArray(3,:),'LineWidth',3)
-hold on
-%plot(0:1:tf,TUNR01(:,3),'*r','LineWidth',3)
-plot(0:1:tf,TUNR02(:,3),'*r','LineWidth',3)
-ylabel('R')
-set(gca,'FontSize',24)
-grid on
-grid minor
-subplot(4,1,4)
-plot(t,xhatArray(4,:),'LineWidth',3)
-hold on
-%plot(0:1:tf,TUNR01(:,4),'*r','LineWidth',3)
-plot(0:1:tf,TUNR02(:,4),'*r','LineWidth',3)
-ylabel('D')
-xlabel('Date');
 set(gca,'FontSize',24)
 grid on
 grid minor
 
 figure(2)
-plot(t,xhatArray(5,:),'LineWidth',3)
+subplot(2,1,1)
+plot(td,[xhatHArray TUNR0(end,3)],'LineWidth',6)
 hold on
-plot(t,mean(xhatArray(5,2000:end))*ones(1,4500),'r','LineWidth',3)
+plot(td,TUNR0(:,3),'*r','LineWidth',6)
+ylabel('R')
+set(gca,'FontSize',24)
+legend('Estimasi','Data Lapangan')
+grid on
+grid minor
+subplot(2,1,2)
+plot(td,[xhatDArray TUNR0(end,4)],'LineWidth',6)
+hold on
+plot(td,TUNR0(:,4)','*r','LineWidth',6)
+ylabel('D')
+xlabel('Tanggal');
+set(gca,'FontSize',24)
+grid on
+grid minor
+
+figure(3)
+semilogy(td,[xhatRArray xhatRArray(end)],'LineWidth',6)
+hold on
+semilogy(td,ones(1,tf),'LineWidth',6)
 title('Estimasi Nilai R0')
 ylabel('R0');
-xlabel('Date');
-legend('R0 dari EKF','Rata2 setelah steady state')
+xlabel('Tanggal');
 set(gca,'FontSize',24)
 grid on
 grid minor
