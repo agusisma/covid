63724b8407c51a945c8c84a6c04c9bbbb08c9a6c
agusisma <62523361+agusisma@users.noreply.github.com>
2020-04-18 10:30:50 +0200

    Update EKFRt.m

--- EKFRt.m
+++ EKFRt.m
@@ -13,7 +13,7 @@ tf  = length(DATA);
 N   = sum(DATA(1,3:end));                    % number of population
 CFR = DATA(end,end)/(sum(DATA(end,4:6)));    % case fatality rate
 td  = datetime(2020,DATA(1,1),DATA(1,2)-1) + caldays(1:tf);
-Ti  = 10;                                    % infection time
+Ti  = 9;                                    % infection time
 
 dt  = 0.01;
 t   = dt:dt:tf;
@@ -32,6 +32,7 @@ Pplus    = 0*eye(5);
 %% Paramater
 gamma  = (1-CFR)*(1/Ti);
 kappa  = CFR*1/Ti;
+alpha1 = 1;
 sigma1 = 1.96; %95 CI
 std_R  = 0.2;
 
@@ -52,7 +53,7 @@ xhatArray  = [];
 for i=1:((tf-1)/dt)
      xhatArray = [xhatArray xhat]; 
      
-     % prediction using discrete-time stochastic augmented compartmental model
+     % prediction
      
      xhat(1) = xhat(1)-(gamma+kappa)*xhat(5)*xhat(1)*xhat(2)*dt/N;
      xhat(2) = xhat(2)+(gamma+kappa)*xhat(5)*xhat(1)*xhat(2)*dt/N-(gamma+kappa)*xhat(2)*dt;
@@ -75,7 +76,8 @@ for i=1:((tf-1)/dt)
     Pmin  = FX*Pplus*FX'+QF;
 
     KF    = Pmin*C'*inv(C*Pmin*C'+RF);
-    
+%    QF    = alpha1*QF + (1-alpha1)*(KF*(y(:,i)-C*xhat)*(y(:,i)-C*xhat)'*KF');    
+
     % update 
     xhat  = xhat + KF*(y(:,i)-C*xhat);
     Pplus = (eye(5)-KF*C)*Pmin;
@@ -108,9 +110,15 @@ for i=1:tf-1
     xhatR      = xhatArray(5,100*i);
 end
 
+xhatSArray = [xhatSArray xhatS];
+xhatIArray = [xhatIArray xhatI];
+xhatHArray = [xhatHArray xhatH];
+xhatDArray = [xhatDArray xhatD];
+xhatRArray = [xhatRArray xhatR];
+
 figure(1)
 subplot(3,1,1)
-plot(td,[xhatIArray DATA(end,4)],'LineWidth',6)
+plot(td,xhatIArray,'LineWidth',6)
 hold on
 plot(td,DATA(:,4),'*r','LineWidth',6)
 ylabel('Active Cases')
@@ -119,7 +127,7 @@ legend('Estimation','Reported Cases')
 grid on
 grid minor
 subplot(3,1,2)
-plot(td,[xhatHArray DATA(end,5)],'LineWidth',6)
+plot(td,xhatHArray,'LineWidth',6)
 hold on
 plot(td,DATA(:,5),'*r','LineWidth',6)
 ylabel('Recovered')
@@ -127,7 +135,7 @@ set(gca,'FontSize',24)
 grid on
 grid minor
 subplot(3,1,3)
-plot(td,[xhatDArray DATA(end,6)],'LineWidth',6)
+plot(td,xhatDArray,'LineWidth',6)
 hold on
 plot(td,DATA(:,6)','*r','LineWidth',6)
 ylabel('Death')
@@ -136,9 +144,8 @@ set(gca,'FontSize',24)
 grid on
 grid minor
 
-H       = [xhatRArray xhatR];
-curve1  = H + sigma1*std_R;
-curve2  = max(H - sigma1*std_R,0);
+curve1  = xhatRArray + sigma1*std_R;
+curve2  = max(xhatRArray - sigma1*std_R,0);
 x2      = [td, fliplr(td)];
 
 figure(2)
@@ -146,7 +153,7 @@ inBetween = [curve1, fliplr(curve2)];
 fill(x2, inBetween, 'k');
 alpha(0.5)
 hold on;
-plot(td,H,'k','LineWidth',6)
+plot(td,xhatRArray,'k','LineWidth',6)
 hold on
 plot(td,ones(1,tf),'r','LineWidth',6)
 title('Daily Reproduction Number (Rt)')
@@ -154,3 +161,19 @@ xlabel('Date');
 set(gca,'FontSize',24)
 grid on
 grid minor
+
+% RMS
+
+% RMSS = sqrt(((xhatSArray)'-DATA(1:end-1,3))'*((xhatSArray)'-DATA(1:end-1,3)))/length(xhatSArray);
+% RMSI = sqrt(((xhatIArray)'-DATA(1:end-1,4))'*((xhatIArray)'-DATA(1:end-1,4)))/length(xhatIArray);
+% RMSR = sqrt(((xhatHArray)'-DATA(1:end-1,5))'*((xhatHArray)'-DATA(1:end-1,5)))/length(xhatHArray);
+% RMSD = sqrt(((xhatDArray)'-DATA(1:end-1,6))'*((xhatDArray)'-DATA(1:end-1,6)))/length(xhatDArray);
+% RMS  = RMSS+RMSI+RMSR+RMSD
+
+for j = 1:tf
+    RMSS = sqrt(((xhatSArray(j)-DATA(j,3))/DATA(j,3))^2);
+    RMSI = sqrt(((xhatIArray(j)-DATA(j,4))/DATA(j,4))^2);
+    RMSH = sqrt(((xhatHArray(j)-DATA(j,5))/DATA(j,5))^2);
+    RMSD = sqrt(((xhatDArray(j)-DATA(j,6))/DATA(j,6))^2);
+    RMS  = (RMSS+RMSI+RMSH+RMSD)*100                        % Percentage of RMS
+end
